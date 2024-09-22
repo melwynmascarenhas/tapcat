c3a176a80d088d6dd0514c8a076d92602ad1a26e
Melwyn <121210177+melwynmascarenhas@users.noreply.github.com>
2024-09-22 15:52:48 +0530

    cleanup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,42 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
-import './styles/style.css'
 
-console.log('Welcome to Vite + JS + Webflow!')
 
-createBadge()
-animateTitle()
+// GIVE UNIQUE IDS TO EACH FILTER
+$(".svg-filter").each(function (index) {
+  $(this).parent().attr("style", "filter: url(#svg-filter-" + index + ");");
+  $(this).find("filter").attr("id", "svg-filter-" + index);
+});
+
+// LOOPING HEADING
+$(".heading-wrap").each(function (index) {
+  let svg = $(this).find(".svg-filter");
+
+  let tl = gsap.timeline({
+	repeat: -1,
+    yoyo: true,
+    defaults: {
+      duration: 1,
+      ease: "power1.out"
+    }
+  });
+  tl.fromTo(svg.find("[stdDeviation]"),
+    { attr: { stdDeviation: 2 } },
+    { attr: { stdDeviation: 7 } });
+});
+
+// ANIMATE IMAGES ON HOVER
+$(".image-wrap").each(function (index) {
+  let svg = $(this).find(".svg-filter");
+
+  let tl = gsap.timeline({ paused: true });
+  tl.fromTo(svg.find("[scale]"),
+    { attr: { scale: 0 } },
+    { attr: { scale: 100 } });
+    
+  $(this).on("mouseenter", function () {
+    tl.play();
+  });
+  $(this).on("mouseleave", function () {
+    tl.reverse();
+  });
+    
+});
